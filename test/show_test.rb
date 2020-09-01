815c3e76c58bc90131c5eeb070f92195b91a058a
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-01 10:17:17 -0500

    Add initial tests for Show class

---
+++ test/show_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+
+class ShowTest < Minitest::Test
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_instance_of Show, knight_rider
+  end
+
+  def test_it_has_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 'Knight Rider', knight_rider.name
+    assert_equal 'Glen Larson', knight_rider.creator
+    assert_equal [michael_knight, kitt], knight_rider.characters
+  end
+end
